164f15e041406c1641a1c757ccd5c2fea59961ec
Scott-19 <henryassistente1@gmail.com>
2025-11-24 09:32:05 +0200

    Create _init_.py

---
+++ core/_init_.py
@@ -0,0 +1,13 @@
+# core/__init__.py
+"""
+Vexus Core Module
+Módulo principal do assistente pessoal Vexus
+"""
+
+__version__ = "1.0.0"
+__author__ = "Vexus Team"
+
+# Exportações principais
+from .vexus import Vexus
+
+__all__ = ['Vexus']
